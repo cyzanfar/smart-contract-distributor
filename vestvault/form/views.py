af076845a4d1447a0ba5f540882c8c4a9c25ce93
Pooja <pkale@purdue.edu>
2018-10-18 19:50:09 -0400

    print token to page

--- vestvault/form/views.py
+++ vestvault/form/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .models import Token
 # Create your views here.
 
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the form index.")
+    t = Token.objects.first()
+    return HttpResponse("Hey {}, your token is {}.".format(t.company_name, t.token_name))
